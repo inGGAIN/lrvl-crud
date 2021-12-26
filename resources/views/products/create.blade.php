88727e63849f3315a385b074841062250bf568ef
inGGAIN _ <inGGAIN@inGGAIN.272>
2021-12-27 07:49:19 +0800

    add input, textarea, back btn, submit

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -2,6 +2,47 @@
 
 @section('content')
 
-<h1>Create Product...</h1>
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Add New Product</h2>
+            </div>
+            <div class="pull-right">
+                <a href="{{ route('products.index') }}" class="btn btn-primary"> Back</a>
+            </div>
+        </div>
+    </div>
 
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <strong>Whoops!</strong> There were some problems with your input. <br><br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    <form action="{{ route('products.store') }}" method="post">
+        @csrf
+
+        <div class="row">
+            <div class="col-xs-12 col-md-12">
+                <div class="form-group">
+                    <strong>Name : </strong>
+                    <input type="text" placeholder="Name" name="name" class="form-control">
+                </div>
+            </div>
+            <div class="col-xs-12 col-md-12">
+                <div class="form-group">
+                    <strong>Detail : </strong>
+                    <textarea style="150px;" type="text" placeholder="Detail" name="detail"
+                        class="form-control"></textarea>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 text-center">
+                <button type="submit" class="btn btn-primary">SUBMIT</button>
+            </div>
+        </div>
+    </form>
 @endsection
