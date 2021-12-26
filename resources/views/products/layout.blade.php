741772f9837e02ce24a4a358112fc6cc6e5c5f72
inGGAIN _ <inGGAIN@inGGAIN.272>
2021-12-27 07:03:14 +0800

    new files: products.create, layout

---
+++ resources/views/products/layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel CRUD</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css">
+</head>
+<body>
+    <h1>Laravel CRUD</h1>
+    <div class="container">
+        <br>
+        @yield('content')
+    </div>
+</body>
+</html>
